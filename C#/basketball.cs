6722ad527d57417a53d4ea953364dddc3ea0d2aa
rsimari <rsimari@nd.edu>
2016-03-01 21:12:14 -0500

    basketball.cs now calculates the conference coefficients

--- C#/basketball.cs
+++ C#/basketball.cs
@@ -9,27 +9,33 @@ using System.Diagnostics;
 /*
 	1: make a file named teams.txt
 	2: fill the file with the format (Team Name) \n (Team ID number from espn.com)
+   3. fill conferenceTop25.txt with the number of each conference in top25
 	3: Run program
   notes: if there is no score or name on the game that means the team is not D1
+
 */
+
 
 namespace Basketball {
 
    class Team {
       private string name;
       private string conference;
-      public float nonConfMargin = 0; // non conf margin
-      public float confMargin = 0;
+      public double nonConfMargin = 0; // non conf margin
+      public double confMargin = 0;
       private string id;
-      public float conferenceCoeff;
+      public double conferenceCoeff;
       private string URL;
       private string STATS_URL; // http://espn.go.com/mens-college-basketball/team/stats/_/id/ + y + /
       public int [] stats = new int [11]; // look at html for order of stats
       private int wins = 0; // total wins
       private int losses = 0; // total losses
       public int topWins = 0; // wins vs top 25
+      public int topLosses = 0; // losses vs top 25
       public int nonConfWins = 0; // non conference wins
+      public int nonConfLosses = 0; // non conference losses
       public int confWins = 0; // conference wins
+      public int confLosses = 0; // conference losses
     	private int[,] games = new int[35,4]; // games[score1, score2, W/L, Rank of Opp]
       public string[] schedule = new string[40]; // filled with teams from their schedule
       public string[] confSchedule = new string[40]; // filled with conferences they played
@@ -61,15 +67,21 @@ namespace Basketball {
          set { stats = value; }
       }
       public void getNonConf() {
-         int i; // margin should include 
+         int i; 
          nonConfWins = 0;
          for (i = 1; i < wins+losses; i++)
-            if (conference != confSchedule[i] && games[i,2] == 1) { // counts only wins and against non conf
-               nonConfWins++; // a non conference win
-               nonConfMargin = nonConfMargin + (games[i,0]-games[i,1]); // also gets the total margin for non conf games
-            } else if (conference == confSchedule[i] && games[i,2] == 1) {
+            if (conference != confSchedule[i]) { // counts only wins and against non conf
+               if (games[i,2] == 1) {
+                  nonConfWins++; // a non conference win
+                  nonConfMargin = nonConfMargin + (games[i,0]-games[i,1]); // also gets the total margin for non conf games
+               } else {
+                  nonConfLosses++; // gets totals of all kinds of wins
+               }
+            } else if (games[i,2] == 1) {
                confMargin = confMargin + (games[i,0]-games[i,1]);
                confWins++;
+            } else {
+               confLosses++;
             }
       }
       public int getConfWins() { return (wins - nonConfWins); }
@@ -139,15 +151,17 @@ namespace Basketball {
          foreach (string y in ranks) {
             t = 0; size = 1;
             if (y[0] == '#') {
-            if (y[2] != ' ') {
-               games[i,3] = (y[1] - '0') * 10 + (y[2] - '0');
-            } else {
-               games[i,3] = y[1] - '0';
-            }
+               if (y[2] != ' ') {
+                  games[i,3] = (y[1] - '0') * 10 + (y[2] - '0');
+               } else {
+                  games[i,3] = y[1] - '0';
+               }
                // check for top 25 win
-            if (games[i,2] == 1)
-               topWins++;
-            }
+               if (games[i,2] == 1)
+                  topWins++;
+               else if (games[i,2] == 0) 
+                  topLosses++;
+            } 
              
              // for the name of the team
             while (y[t] != '>') 
@@ -310,7 +324,7 @@ namespace Basketball {
          // closes the teams.txt file
          sr.Close(); 
 
-         Dictionary<string, float> coeff = new Dictionary<string, float>(); // dictionary iwth all athe conf coefficients
+         Dictionary<string, double> coeff = new Dictionary<string, double>(); // dictionary iwth all athe conf coefficients
          sr = File.OpenText("conference2.txt");
          string key;
          // makes empty dictionary of just the conferences -> 0
@@ -322,8 +336,10 @@ namespace Basketball {
 
          sr.Close(); // closes conference2.txt
 
+         // conf coeff = log(totalMargin^(teams in top 25)*(wins% against top 25)*(non conference win %))
          // fills dictionary with total margin for each conference
          for (i = 0; i < 10; i++) {
+            //Console.WriteLine(teamArray[i].confWins);
             if (coeff.ContainsKey(teamArray[i].Conference)) {
                coeff[teamArray[i].Conference] += teamArray[i].nonConfMargin;
             } else {
@@ -331,8 +347,31 @@ namespace Basketball {
             }
          }
 
-         // need to put the confCoeff in each team???? i can compare the two conferences without 
-         // putting the confCoeff as a member variable. just look it up in Coeff dictionary
+         // ^(teams in top 25)
+         sr = File.OpenText("conferenceTop25.txt");
+         string team;
+         double number;
+         for (i = 0; i < 32; i++) {
+            team = sr.ReadLine(); // conference name
+            number = Int32.Parse(sr.ReadLine()); // # of top 25 teams in conference
+            coeff[team] = (Math.Pow(coeff[team], number)); // maths
+         }
+
+         double wins = 0, winsB = 0, total = 0, totalB = 0;
+         foreach (var k in coeff.Keys.ToList()) {
+            for (i = 0; i < 10; i++) {
+               if (teamArray[i].Conference == k) {
+                  wins += teamArray[i].topWins;
+                  total += (teamArray[i].topWins + teamArray[i].topLosses);
+                  winsB += teamArray[i].nonConfWins;
+                  totalB += (teamArray[i].nonConfWins + teamArray[i].nonConfLosses);  
+               }
+            }
+            if (total != 0) {
+               coeff[k] = Math.Log(coeff[k] * (wins/total * winsB/totalB), 10); // maths
+            }
+            wins = 0; winsB = 0; total = 0; totalB = 0; // reset
+         }
          
          // writes to out.txt
          StreamWriter writer = new StreamWriter("out.txt");
